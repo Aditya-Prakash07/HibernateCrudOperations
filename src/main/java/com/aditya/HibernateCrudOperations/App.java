5a89243c69f13179eee5a567756754e277fb1acc
Aditya-Prakash07 <prakashaditya775@gmail.com>
2025-01-14 18:51:43 +0530

    CRUD Operations Using Hibernate Completed..(_)

--- src/main/java/com/aditya/HibernateCrudOperations/App.java
+++ src/main/java/com/aditya/HibernateCrudOperations/App.java
@@ -9,12 +9,12 @@ import com.aditya.entities.User;
 
 public class App {
 	public static void main(String[] args) {
-		User user1 = new User();
-		user1.setName("Mai Khud");
-		user1.setCity("Hajipur");
-		user1.setGender("male");
-		user1.setEmail("prakashaditya775@gmail.com");
-		user1.setPassword("password@123");
+//		User user3 = new User();
+//		user3.setName("Priya");
+//		user3.setCity("Hajipur");
+//		user3.setGender("female");
+//		user3.setEmail("priyawa@gmail.com");
+//		user3.setPassword("password@priyawa");
 
 		Configuration cfg = new Configuration();
 		cfg.configure("/com/aditya/config/hibernate.cfg.xml");
@@ -22,15 +22,71 @@ public class App {
 																	// heavy weight so we should not create it again and
 																	// again
 		Session session = sessionFactory.openSession(); // it will provide CRUD operations methods
-		Transaction transaction = session.beginTransaction(); // it will provide transactions eiter all or none
+		Transaction transaction = session.beginTransaction(); // it will provide transactions eiter all or none . Needed
+																// for insert update and delete operations not for get
+																// opetations
+
+//		 ----------------------Insert Operation----------------------
+
+//		try {
+//			session.save(user3);
+//			transaction.commit();
+//			System.out.println("  User Details Added Successfully...!  ");
+//		} catch (Exception e) {
+//			transaction.rollback();
+//			e.printStackTrace();
+//			System.out.println("  User Details Not Added Due To Some ERROR  ");
+//		}
+
+//		-----------------------Select Operation-----------------------
+
+//		try {
+//			User user = session.get(User.class, 4);
+//			if (user != null) {
+//				System.out.println(user.getId());
+//				System.out.println(user.getName());
+//				System.out.println(user.getEmail());
+//				System.out.println(user.getGender());
+//				System.out.println(user.getPassword());
+//				System.out.println(user.getCity());
+//			}
+//			System.out.println("User not found..");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.out.println("Failed to faitch Data due to some ERROR ");
+//		}
+
+//		-----------------------Update Operation--------------------------
+
+//		try {
+//			User user = session.get(User.class, 2);
+//			user.setCity("Banglore");
+//			session.saveOrUpdate(user);
+//			transaction.commit();
+//			System.out.println("  User Details Updated Successfully...!  ");
+//		} catch (Exception e) {
+//			transaction.rollback();
+//			e.printStackTrace();
+//			System.out.println("  User Details Not Updated Due To Some ERROR  ");
+//		}
+//		
+		
+//		-----------------------Delete Operation--------------------------
 
 		try {
-			session.save(user1);
+			User user = new User();
+			user.setId(3);
+			if(user!=null) {
+			session.delete(user);
 			transaction.commit();
+			System.out.println("  User Deleted Successfully...!  ");
+			}
+			System.out.println("User not found...");
 		} catch (Exception e) {
 			transaction.rollback();
 			e.printStackTrace();
-
+			System.out.println("  Cannot Delete User Due To Some ERROR  ");
 		}
+
 	}
 }
